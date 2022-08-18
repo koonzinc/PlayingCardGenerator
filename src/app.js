5ee2e8f13fae7142bcc97676ba94ddc674208df3
koonzinc <koonz.eth@gmail.com>
2022-08-17 21:14:29 -0400

    first commit

---
+++ src/app.js
@@ -0,0 +1,20 @@
+import "./style.css";
+
+window.onload = function() {
+  document.querySelector('.card ').classList.add(generateRandomSuit);
+  document.querySelector('.cardNumber').innerHTML = (generateRandomNumber);
+};
+
+let generateRandomNumber = () => {
+  let numbers = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+  let numbersIndex = Math.floor(Math.random() * numbers.length);
+
+  return numbers[numbersIndex];
+}
+
+let generateRandomSuit = () => {
+  let suit = ["spade", "club", "heart", "diamond"];
+  let suitIndex = Math.floor(Math.random() * suit.length);
+
+  return suit[suitIndex];
+}
