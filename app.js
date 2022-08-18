b0396e34e55d1278bb488807a77a898afe4caa7b
koonzinc <koonz.eth@gmail.com>
2022-08-18 10:49:50 -0400

    final commit

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 let generateRandomNumber = () => {
   let numbers = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
   let numbersIndex = Math.floor(Math.random() * numbers.length);
@@ -17,5 +16,4 @@ window.onload = () => {
     document.querySelector('.card').classList.remove('spade');
     document.querySelector('.card').classList.add(generateRandomSuit());
     document.querySelector('.cardNumber').innerHTML = generateRandomNumber();
-  
 };
